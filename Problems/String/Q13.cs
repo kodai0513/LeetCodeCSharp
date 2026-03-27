d7e0ebb04f2828e0f02338852dd5858041ed865f
kodai0513 <arare030513@gmail.com>
2026-03-27 21:00:00 +0900

    String - Q13

--- Problems/String/Q13.cs
+++ Problems/String/Q13.cs
@@ -7,6 +7,35 @@ namespace LeetCodeCSharp.Problems.String
 {
     public class Q13
     {
-        
+        public int RomanToInt(string s)
+        {
+            var romanMap = new Dictionary<char, int>
+            {
+                ['I'] = 1,
+                ['V'] = 5,
+                ['X'] = 10,
+                ['L'] = 50,
+                ['C'] = 100,
+                ['D'] = 500,
+                ['M'] = 1000
+            };
+
+            var ans = 0;
+            for(var i = 0; i < s.Length; i++)
+            {
+                if(i + 1 < s.Length && romanMap[s[i]] < romanMap[s[i + 1]])
+                {
+                    ans -= romanMap[s[i]];
+                }
+                else
+                {
+                    ans += romanMap[s[i]];
+                }
+                
+            }
+
+            return ans;
+
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿using LeetCodeCSharp.Problems;
-using LeetCodeProject.Problems;
+﻿using LeetCodeCSharp.Problems.String;
+
+var ans = new Q13().RomanToInt("MCMXCIV");
+Console.WriteLine(ans);
